9f7893e0857964e9fa12e561a80ffc4603fc59fd
MahmoudAbdelazim <mahmoud2011989@gmail.com>
2023-02-26 19:08:15 +0200

    Add Speakers List Cards

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import { data } from "../SpeakerData";
+import Speaker from "../src/components/speaker";
+
+const IndexPage = () => {
+  return (
+    <div className="container speakers-list">
+      <div className="row">
+        {data.map((speaker) => {
+          return <Speaker key={speaker.id} speaker={speaker} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default IndexPage;
---
+++ src/components/Speaker.js
@@ -0,0 +1,73 @@
+const Session = (props) => {
+  const { title, room } = props;
+  return (
+    <span className="session w-100">
+      {title} <strong>Room: {room} </strong>
+    </span>
+  );
+};
+
+const Sessions = ({ sessions }) => {
+  return (
+    <div className="sessionBox card h-250">
+      <Session title={sessions[0].title} room={sessions[0].room.name} />
+    </div>
+  );
+};
+
+const SpeakerImage = ({ id, first, last }) => {
+  return (
+    <div className="speaker-img d-flex flex-row justify-content-center align-items-center">
+      <img
+        className="contain-fit"
+        src={`/images/speaker-${id}.jpg`}
+        width="300"
+        alt={`${first} ${last}`}
+      />
+    </div>
+  );
+};
+
+const SpeakerDemographics = ({ speaker }) => {
+  const { first, last, bio, company, twitterHandle } = speaker;
+  return (
+    <div className="speaker-info">
+      <div className="d-flex justify-content-between mb-3">
+        <h3 className="text-truncate w-200">
+          {first} {last}
+        </h3>
+      </div>
+      <div>
+        <p className="card-description">{bio}</p>
+        <div className="social d-flex flex-row mt-4">
+          <div className="company">
+            <h5>Company</h5>
+            <h6>{company}</h6>
+          </div>
+          <div className="twitter">
+            <h5>Twitter</h5>
+            <h6>{twitterHandle}</h6>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Speaker = ({ speaker }) => {
+  return (
+    <div className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-sm-12 col-xs-12">
+      <div className="card card-height p-4 mt-4 speaker-card">
+        <SpeakerImage
+          id={speaker.id}
+          first={speaker.first}
+          last={speaker.last}
+        />
+        <SpeakerDemographics speaker={speaker} />
+        <Sessions sessions={speaker.sessions} />
+      </div>
+    </div>
+  );
+};
+
+export default Speaker;
